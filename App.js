332647bce328cbb6b232c3f196843e5096af1174
Vaibhav Mehar <vaibhavmehar16@gmail.com>
2022-01-25 13:59:01 +0530

    updated 90% homescreen

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import Home from "./screens/Home";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <Home />
   );
 }
---
+++ components/HeaderTabs.js
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+
+export default function HeaderTabs() {
+    const [activeTab, setactiveTab] = useState("Delivery");
+  return (
+    <View style={styles.container}>
+      <HeaderButton text='Delivery' btnColor="black" textColor="white" activeTab={activeTab} setactiveTab={setactiveTab} />
+      <HeaderButton text='Pickup' btnColor="white" textColor="black" activeTab={activeTab} setactiveTab={setactiveTab}/>
+    </View>
+  );
+}
+
+const HeaderButton = (props) => (
+  <View>
+    <TouchableOpacity style={{
+        backgroundColor: props.activeTab === props.text ? 'black' : 'white',
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30,
+    }} onPress={() => props.setactiveTab(props.text)}>
+      <Text style={{color: props.activeTab === props.text ? 'white' : 'black', fontSize: 16, fontWeight:'700' }}>{props.text}</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    alignSelf: 'center',
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+});
